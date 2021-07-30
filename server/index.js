f3631b20dc3bfdde48947037f7e7b24e3a23c36f
Caio Nóbile <caio.nobile51@gmail.com>
2021-07-30 00:04:07 -0300

    Creating "Posts" table using sequelize

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const db = require("./models");
+
 const app = express();
 
-app.listen(3001, () => {
-  console.log("Server running on port 3001");
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
 });
