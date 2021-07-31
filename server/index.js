9aa6e898fe9e9b7cf86cddb639c0aabcbb86cefa
Caio Nóbile <caio.nobile51@gmail.com>
2021-07-31 01:17:50 -0300

    Using cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const db = require("./models");
 const postRouter = require("./routes/Posts");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {
