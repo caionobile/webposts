ea01c5c3dac8a1710b700e52a779b7469bd7aca2
Caio Nóbile <caio.nobile51@gmail.com>
2021-08-05 00:30:40 -0300

    Creating table of users

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("./models");
 const postRouter = require("./routes/Posts");
 const commentsRouter = require("./routes/Comments");
+const usersRouter = require("./routes/Users");
 const cors = require("cors");
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/posts", postRouter);
 app.use("/comments", commentsRouter);
+app.use("/auth", usersRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
---
+++ server/routes/Users.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+
+router.get("/", async (req, res) => {
+  const allUsers = await Users.findAll();
+  res.status(200).json(allUsers);
+});
+
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({ username: username, password: hash });
+  });
+  res.status(201).json({ message: "User created" });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await Users.findOne({ where: { username: username } });
+    if (!user)
+      return res.status(400).json({
+        error: "Username or password is incorrect. Authentication failed",
+      });
+    bcrypt.compare(password, user.password).then((match) => {
+      if (!match)
+        return res.status(400).json({
+          error: "Username or password is incorrect. Authentication failed",
+        });
+
+      return res.status(200).json({
+        message: "Success",
+      });
+    });
+  } catch (e) {
+    return res.status(400).json({
+      error: e,
+    });
+  }
+});
+
+router.patch("/:id", async (req, res) => {});
+
+module.exports = router;
